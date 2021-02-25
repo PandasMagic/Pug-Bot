5a875f28dac63cd0bc08db15f55c7c7f14006abf
Marvin <marvthedonut@hotmail.com>
2021-02-25 10:56:24 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+const fs = require('fs');
 
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+client.commands = new Discord.Collection();
 for (const file of commandFiles) {
   const command = require(`./commands/${file}`);
   client.commands.set(command.name, command);
@@ -12,7 +14,7 @@ const prefix = "!"
 client.on('message', (message) => {
     if(!message.content.startsWith(prefix)) return;
     const args = message.content.slice(prefix.length).trim().split(/ +/);
-    const command = client.commands.get(args[0]) || client.commands.find(cmd => cmd.aliases && cmd.aliases.toLowerCase() == args[0].toLowerCase());
+    const command = client.commands.get(args[0]) || client.commands.find(cmd => cmd.aliases && cmd.aliases.find(alias => alias.toLowerCase() == args[0].toLowerCase()));
     if(!command) message.reply(`No such command found for \`${Discord.Util.cleanContent(prefix + args[0].toLowerCase(), message)}\``);
     command.execute(client, Discord, message, args);
 });
