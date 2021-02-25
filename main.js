52e196289573f58b20aa38ab4999f9f02ac276f2
Marvin <marvthedonut@hotmail.com>
2021-02-25 10:38:24 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.name, command);
+}
+
+const prefix = "!"
 
 client.on('message', (message) => {
-    
-    if(message.content === 'test') {
-        message.reply('test');
-        msg.channel.send(`test`);
-    }
-
+    if(!message.content.startsWith(prefix)) return;
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const command = client.commands.get(args[0]) || client.commands.find(cmd => cmd.aliases && cmd.aliases.toLowerCase() == args[0].toLowerCase());
+    if(!command) message.reply(`No such command found for \`${Discord.Util.cleanContent(prefix + args[0].toLowerCase(), message)}\``);
+    command.execute(client, Discord, message, args);
 });
 
 client.login('token');
